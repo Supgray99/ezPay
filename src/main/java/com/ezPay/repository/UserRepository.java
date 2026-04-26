3664d6b5c225b6998db8096d1cd30fa5bbcf6bd4
Supgray99 <68681647+Supgray99@users.noreply.github.com>
2026-04-26 22:17:50 +0530

    Committing new endpoints for AI module to invoke

--- src/main/java/com/ezPay/repository/UserRepository.java
+++ src/main/java/com/ezPay/repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.ezPay.repository;
 import com.ezPay.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
